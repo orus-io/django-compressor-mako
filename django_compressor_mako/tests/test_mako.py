23f2f425946b348cd47e857883c6547fab3e8542
Steeve Chailloux <steeve@chaahk.com>
2017-12-15 16:05:06 +0100

    [fix] pep8 on test file

--- django_compressor_mako/tests/test_mako.py
+++ django_compressor_mako/tests/test_mako.py
@@ -20,15 +20,18 @@ class TestMakoCompressor(TestCase):
             '<%namespace name="compress" module="django_compressor_mako"/>',
             '<%compress:' + tag + '>',
             body,
-            '</%compress:' + 'compress>' if 'kind' in tag else '</%compress:' + tag + '>',
+            '</%compress:' + 'compress>'
+            if 'kind' in tag else '</%compress:' + tag + '>',
         ])
 
     @override_settings(COMPRESS_ENABLED=False)
     def test_compress_is_disabled(self):
         tag_body = '\n'.join([
-            '<link rel="stylesheet" href="css/one.css" type="text/css" charset="utf-8">',
+            '<link rel="stylesheet" href="css/one.css" '
+            'type="text/css" charset="utf-8">',
             '<style type="text/css">p { border:5px solid green;}</style>',
-            '<link rel="stylesheet" href="css/two.css" type="text/css" charset="utf-8">',
+            '<link rel="stylesheet" href="css/two.css" '
+            'type="text/css" charset="utf-8">',
         ])
         template_string = self.wrap(tag_body, 'compress kind="css"')
         template = self.template(template_string)
@@ -41,9 +44,11 @@ class TestMakoCompressor(TestCase):
 
     def test_css_tag(self):
         template = self.template(self.wrap(''.join([
-            '<link rel="stylesheet" href="${ STATIC_URL }css/one.css" type="text/css" charset="utf-8">',
+            '<link rel="stylesheet" href="${ STATIC_URL }css/one.css" '
+            'type="text/css" charset="utf-8">',
             '<style type="text/css">p { border:5px solid green;}</style>',
-            '<link rel="stylesheet" href="${ STATIC_URL }css/two.css" type="text/css" charset="utf-8">',
+            '<link rel="stylesheet" href="${ STATIC_URL }css/two.css" '
+            'type="text/css" charset="utf-8">',
             ]), 'compress kind="css"'))
         context = {'STATIC_URL': settings.COMPRESS_URL}
         out = css_tag("/static/CACHE/css/58a8c0714e59.css")
@@ -51,7 +56,8 @@ class TestMakoCompressor(TestCase):
 
     def test_nonascii_css_tag(self):
         template = self.template(self.wrap(''.join([
-            '<link rel="stylesheet" href="${ STATIC_URL }css/nonasc.css" type="text/css" charset="utf-8">',
+            '<link rel="stylesheet" href="${ STATIC_URL }css/nonasc.css" '
+            'type="text/css" charset="utf-8">',
             '<style type="text/css">p { border:5px solid green;}</style>',
             ]), 'css'))
         context = {'STATIC_URL': settings.COMPRESS_URL}
@@ -60,34 +66,44 @@ class TestMakoCompressor(TestCase):
 
     def test_js_tag(self):
         template = self.template(self.wrap(''.join([
-            '<script src="${ STATIC_URL }js/one.js" type="text/javascript" charset="utf-8"></script>',
-            '<script type="text/javascript" charset="utf-8">obj.value = "value";</script>',
+            '<script src="${ STATIC_URL }js/one.js" '
+            'type="text/javascript" charset="utf-8"></script>',
+            '<script type="text/javascript" '
+            'charset="utf-8">obj.value = "value";</script>',
             ]), 'compress kind="js"'))
         context = {'STATIC_URL': settings.COMPRESS_URL}
-        out = '<script type="text/javascript" src="/static/CACHE/js/74e158ccb432.js"></script>'
+        out = '<script type="text/javascript" ' \
+            'src="/static/CACHE/js/74e158ccb432.js"></script>'
         self.assertEqual(out, template.render(**context))
 
     def test_nonascii_js_tag(self):
         template = self.template(self.wrap(''.join([
-            '<script src="${ STATIC_URL }js/nonasc.js" type="text/javascript" charset="utf-8"></script>',
-            '<script type="text/javascript" charset="utf-8">var test_value = "\u2014";</script>',
+            '<script src="${ STATIC_URL }js/nonasc.js" '
+            'type="text/javascript" charset="utf-8"></script>',
+            '<script type="text/javascript" '
+            'charset="utf-8">var test_value = "\u2014";</script>',
             ]), 'compress kind="js"'))
         context = {'STATIC_URL': settings.COMPRESS_URL}
-        out = '<script type="text/javascript" src="/static/CACHE/js/a18195c6ae48.js"></script>'
+        out = '<script type="text/javascript" ' \
+            'src="/static/CACHE/js/a18195c6ae48.js"></script>'
         self.assertEqual(out, template.render(**context))
 
     def test_nonascii_latin1_js_tag(self):
         template = self.template(self.wrap(''.join([
-            '<script src="${ STATIC_URL }js/nonasc-latin1.js" type="text/javascript" charset="latin-1"></script>',
-            '<script type="text/javascript">var test_value = "\u2014";</script>',
+            '<script src="${ STATIC_URL }js/nonasc-latin1.js" '
+            'type="text/javascript" charset="latin-1"></script>',
+            '<script type="text/javascript">'
+            'var test_value = "\u2014";</script>',
             ]), 'js'))
         context = {'STATIC_URL': settings.COMPRESS_URL}
-        out = '<script type="text/javascript" src="/static/CACHE/js/f64debbd8878.js"></script>'
+        out = '<script type="text/javascript" ' \
+            'src="/static/CACHE/js/f64debbd8878.js"></script>'
         self.assertEqual(out, template.render(**context))
 
     def test_css_inline(self):
         template = self.template(self.wrap(''.join([
-            '<link rel="stylesheet" href="${ STATIC_URL }css/one.css" type="text/css" charset="utf-8">',
+            '<link rel="stylesheet" href="${ STATIC_URL }css/one.css" '
+            'type="text/css" charset="utf-8">',
             '<style type="text/css">p { border:5px solid green;}</style>',
             ]), 'compress kind="css" mode="inline"'))
         context = {'STATIC_URL': settings.COMPRESS_URL}
@@ -99,18 +115,23 @@ class TestMakoCompressor(TestCase):
 
     def test_js_inline(self):
         template = self.template(self.wrap(''.join([
-            '<script src="${ STATIC_URL }js/one.js" type="text/css" type="text/javascript" charset="utf-8"></script>',
-            '<script type="text/javascript" charset="utf-8">obj.value = "value";</script>',
+            '<script src="${ STATIC_URL }js/one.js" type="text/css" '
+            'type="text/javascript" charset="utf-8"></script>',
+            '<script type="text/javascript" charset="utf-8">'
+            'obj.value = "value";</script>',
             ]), 'compress kind="js" mode="inline"'))
         context = {'STATIC_URL': settings.COMPRESS_URL}
-        out = '<script type="text/javascript">;obj={};;obj.value="value";</script>'
+        out = '<script type="text/javascript">' \
+            ';obj={};;obj.value="value";</script>'
         self.assertEqual(out, template.render(**context))
 
     def test_named_css(self):
         template = self.template(self.wrap(''.join([
-            '<link rel="stylesheet" href="${ STATIC_URL }css/one.css" type="text/css" charset="utf-8">',
+            '<link rel="stylesheet" href="${ STATIC_URL }css/one.css" '
+            'type="text/css" charset="utf-8">',
             '<style type="text/css">p { border:5px solid green;}</style>',
-            '<link rel="stylesheet" href="${ STATIC_URL }css/two.css" type="text/css" charset="utf-8">',
+            '<link rel="stylesheet" href="${ STATIC_URL }css/two.css" '
+            'type="text/css" charset="utf-8">',
             ]), 'compress kind="css" mode="file" name="cute"'))
         context = {'STATIC_URL': settings.COMPRESS_URL}
         self.assertRaises(NotImplementedError, template.render, **context)
